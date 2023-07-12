c58cfe25ea89ddfff9a1961a216be8c34028af9d
therealwebdeveloper <realwebdeveloper1@gmail.com>
2023-07-12 19:39:30 +0530

    first commit

---
+++ src/components/Student.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Student() {
+    return <h2>Name: John Due</h2>
+}
+
+export default Student;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Student from './components/Student';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello</h1>
+    <Student />
+  </React.StrictMode>
+);
